d6456538119b88db1ea927d6cd8ac790fb5e3e86
Masayoshi Kojima <kors.webworks@gmail.com>
2017-04-30 11:50:24 +0900

    'Add_queris_playground_file'

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,44 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '590549f252429434806caf3ba';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// }
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
+
+
+// User.findById 59046240228e811258d745a8
+
+
+
+User.findById('59046240228e811258d745a8').then((user) => {
+    if (!user) {
+        return console.log('Unable to find user');
+    }
+    console.log(JSON.stringify(user, undefined, 2));
+},(e) => {
+    console.log(e);
+});
