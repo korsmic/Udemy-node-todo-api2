f756d454664b1c4d0c7f14fcdb314fb7852dd39f
Masayoshi Kojima <kors.webworks@gmail.com>
2017-04-30 17:22:50 +0900

    'Setup_separate_test_and_development_env'

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -142,8 +142,7 @@ describe('DELETE /todos/:id', () => {
   });
 });
 
-
-describe('PATCH /todos:id', () => {
+describe('PATCH /todos/:id', () => {
   it('should update the todo', (done) => {
     var hexId = todos[0]._id.toHexString();
     var text = 'This should be the new text';
@@ -161,7 +160,7 @@ describe('PATCH /todos:id', () => {
         expect(res.body.todo.completedAt).toBeA('number');
       })
       .end(done);
-    });
+  });
 
   it('should clear completedAt when todo is not completed', (done) => {
     var hexId = todos[1]._id.toHexString();
